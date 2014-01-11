e5cf988f5aa1bcb2cbf27b8112d237279c5504f3
Michał Maciej Różański <michal.rozanski@gmail.com>
2014-01-12 00:53:54 +0100

    World create function

--- YetAnotherBallGame.py
+++ YetAnotherBallGame.py
@@ -4,19 +4,25 @@ from direct.actor.Actor import Actor
 from panda3d.bullet import BulletWorld, BulletPlaneShape, BulletRigidBodyNode
 from panda3d.core import Vec3
 
+from world import World
+
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        #self.world = BulletWorld()
-        #self.world.setGravity(Vec3(0, 0, -9.81))
+        w = World
+        w.world_create(100, 100)
 
-        #self.taskMgr.add(self.update, 'update')
+        """
+        self.world = BulletWorld()
+        self.world.setGravity(Vec3(0, 0, -9.81))
 
-        #self.shape = BulletPlaneShape(Vec3(0, 0, 1), 1)
+        self.taskMgr.add(self.update, 'update')
 
-        #self.node = BulletRigidBodyNode('Floor')
-        #self.node.addShape(self.shape)
+        self.shape = BulletPlaneShape(Vec3(0, 0, 1), 1)
+
+        self.node = BulletRigidBodyNode('Floor')
+        self.node.addShape(self.shape)
 
         # Load model
         self.ground = self.loader.loadModel("Models/floor_basic.egg")
@@ -25,18 +31,20 @@ class Game(ShowBase):
         self.ground.reparentTo(self.render)
 
         # Apply scale and position transforms on the model.
-        self.ground.setScale(100, 100, 0)
-        self.ground.setPos(0, -1, 0)
+        self.ground.setScale(1000, 1000, 0)
+        self.ground.setPos(0, 0, 0)
 
-        #self.np = self.render.attachNewNode(self.node)
-        #self.np.setPos(0, 0, -2)
+        self.np = self.render.attachNewNode(self.node)
+        self.np.setScale(1000, 1000, 0)
+        self.np.setPos(0, 0, 0)
 
-        #self.world.attachRigidBody(self.node)
+        self.world.attachRigidBody(self.node)
 
     def update(self, task):
         dt = globalClock.getDt()
         self.world.doPhysics(dt, 10, 1.0/180.0)
         return Task.cont
+        """
 
 app = Game()
 app.run()
